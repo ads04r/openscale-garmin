c7dbad8a9846e3bc3489b250231705d4ba9e7b66
Ash <ash@drashsmith.com>
2020-05-24 01:12:54 +0100

    Basic parser object

---
+++ openscalecsv.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import os, sys, datetime, csv
+
+class OpenScaleCSV(object):
+
+	def __init__(self):
+
+		self.items = []
+
+	def __parsedate__(self, ds):
+
+		dsa = ds.replace(':', ' ').replace('/', ' ').replace('-', ' ').replace('.', ' ').replace('  ', ' ').split(' ')
+		while len(dsa) < 6:
+			dsa.append('0')
+		return datetime.datetime(int(dsa[0]), int(dsa[1]), int(dsa[2]), int(dsa[3]), int(dsa[4]), int(dsa[5]))
+
+	def load(self, filename):
+
+		headers = []
+		with open(filename, 'r') as fp:
+			csvreader = csv.reader(fp, delimiter=',', quotechar='"')
+			for row in csvreader:
+				if len(headers) == 0:
+					headers = row
+					continue
+				item = {}
+				for i in range(0, len(row)):
+					key = headers[i]
+					value = row[i]
+					if key == 'dateTime':
+						value = self.__parsedate__(value)
+						item[key] = value
+						continue
+					if key == 'comment':
+						item[key] = value
+						continue
+					value = float(value)
+					if value != 0.0:
+						item[key] = value
+				self.items.append(item)
+
+	def dump(self):
+
+		return self.items
